a424019b1f82b88d1d66791e14bdb247d2da7df2
Michele Comolli <mikicomolli@gmail.com>
2021-01-12 09:21:21 +0100

    Added Bookings model to the schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_11_181012) do
+ActiveRecord::Schema.define(version: 2021_01_12_081948) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "start_date"
+    t.datetime "end_date"
+    t.string "ship_to_address"
+    t.float "total_price"
+    t.bigint "product_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["product_id"], name: "index_bookings_on_product_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "brands", force: :cascade do |t|
     t.string "name"
@@ -65,6 +78,8 @@ ActiveRecord::Schema.define(version: 2021_01_11_181012) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "products"
+  add_foreign_key "bookings", "users"
   add_foreign_key "products", "brands"
   add_foreign_key "products", "categories"
   add_foreign_key "products", "users"
