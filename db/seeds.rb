9abb5ab19ef56d1561361c17356885b377546e37
Katrina Robles <katrinarobles@live.com>
2021-01-12 14:51:51 +0100

    Created 41 seeds for Brand

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+#  ---- KAT ----------
+BRANDNAMES = [
+  "Advert Tech", "AgfaPhoto", "Aigo", "Bell & Howell", "BenQ", "Blackmagic Design", "Canon", "Casio",
+  "DJI", "Epson", "Foscam", "Fujifilm", "GE" , "Genius", "GoPro", "Hasselblad", "HP", "Kodak", "Leica",
+  "Lytro", "Medion", "Memoto", "Minox", "Nikon", "Olympus", "Panasonic", "Pentax", "Phase One", "Polaroid",
+  "Praktica", "Ricoh", "Rollei", "Samsung", "Sigma", "Sony", "Tevion", "Thomson", "Traveler", "Vageeswari",
+  "VisionTek", "Vivitar"
+]
+
+puts "Creating brand names..."
+BRANDNAMES.each do |brand|
+    Brand.create(name: brand)
+    puts Brand.last.name
+end
+puts "Finished Brand!"
