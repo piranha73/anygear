b837fe11c7683027a9a32c327ea8585ccdb8d5d6
Michele Comolli <mikicomolli@gmail.com>
2021-01-16 00:26:19 +0100

    added associations in brand and mount_type

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,3 +1,4 @@
 class Brand < ApplicationRecord
   has_many :products
+  has_many :mount_types
 end
--- app/models/mount_type.rb
+++ app/models/mount_type.rb
@@ -1,3 +1,4 @@
 class MountType < ApplicationRecord
   belongs_to :brand
+  has_many :lenses
 end
