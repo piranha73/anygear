64fa9e353707bf2cc153f966d05a0df3f489756f
Michele Comolli <mikicomolli@gmail.com>
2021-01-15 01:37:52 +0100

    Lense_type model created

---
+++ app/models/lense_type.rb
@@ -0,0 +1,2 @@
+class LenseType < ApplicationRecord
+end
---
+++ test/models/lense_type_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class LenseTypeTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
