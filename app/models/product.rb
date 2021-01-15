40421b149b06a981b1fbdc6095faaf48333b297d
Michele Comolli <mikicomolli@gmail.com>
2021-01-16 00:41:57 +0100

    added images to product

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,6 @@ class Product < ApplicationRecord
   belongs_to :user
   belongs_to :productable, polymorphic: true
   belongs_to :brand
+  has_one_attached :cover_picture
+  has_many_attached :uploaded_pictures
 end
