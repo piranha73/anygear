9689f27d82672542278aad69261463630a469310
Michele Comolli <mikicomolli@gmail.com>
2021-01-15 01:37:33 +0100

    removed Lense model

--- app/models/lense.rb
+++
@@ -1,4 +0,0 @@
-class Lense < ApplicationRecord
-  belongs_to :camera_lense_brand
-  belongs_to :mount_type
-end
--- test/models/lense_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class LenseTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
