be67e41bcfbc66bc8361287bc2c1c9fe5b933cb1
Michele Comolli <mikicomolli@gmail.com>
2021-01-16 00:16:20 +0100

    stored product_types ad array

--- db/migrate/20210115173031_create_brands.rb
+++ db/migrate/20210115173031_create_brands.rb
@@ -1,7 +1,7 @@
 class CreateBrands < ActiveRecord::Migration[6.0]
   def change
     create_table :brands do |t|
-      t.array :product_types
+      t.string :product_types, array: true
       t.string :name
 
       t.timestamps
