001cb18821acb243fbb6063185def59a5cd41e1a
Michele Comolli <mikicomolli@gmail.com>
2021-01-16 01:03:13 +0100

    created a separate file to store the hash

---
+++ db/brands_hash.rb
@@ -0,0 +1,244 @@
+BRANDS = {
+  "Arri": {
+    cameras: [],
+    lenses: [],
+    mount_types: ['PL']
+  },
+  "Canon": {
+    cameras: [],
+    lenses: [],
+    mount_types: ["EF", "RF", "EF-S", "EF-M"]
+  },
+  "DJI": {
+    cameras: [],
+    lenses: [],
+    stabilizers: [],
+  },
+  "Fujifilm": {
+    cameras: [],
+    lenses: [],
+    mount_types: [ "Fuji G", "Fuji X" ]
+  },
+  "GoPro": {
+    cameras: [],
+    lenses: [],
+  },
+  "Hasselblad": {
+    cameras: [],
+    lenses: [],
+    mount_types: ["Hasselblad X", "Hasselblad H"]
+  },
+  "Insta360": {
+    cameras: [],
+    lenses: [],
+  },
+  "Leica": {
+    cameras: [],
+    lenses: [],
+    mount_types: ["L", "M", "R"]
+  },
+  "Nikon": {
+    cameras: [],
+    lenses: [],
+    mount_types: ["F", "S", "1", "Z"]
+  },
+  "Olympus": {
+    cameras: [],
+    lenses: [],
+    mount_types: ["Pen F", "OM", "Four Thirds", "Micro Four Thirds (MFT)"]
+  },
+  "Panasonic": {
+    cameras: [],
+    lenses: [],
+    mount_types: [ "Micro Four Thirds (MFT)" ]
+  },
+  "Pentax": {
+    cameras: [],
+    lenses: [],
+    mount_types: ["K", "Q"]
+  },
+  "Sony": {
+    cameras: [],
+    lenses: [],
+    mount_types: ["E", "EX", "A"]
+  },
+  "Sigma": {
+    cameras: [],
+    lenses: [],
+  },
+  # start audio brands
+  "Diety": {
+    audios: []
+  },
+  "Rode": {
+    audios: []
+  },
+  "Blue Yeti": {
+    audios: []
+  },
+  "Audio Technica": {
+    audios: []
+  },
+  "Sennheiser": {
+    audios: []
+  },
+  "Countryman": {
+    audios: []
+  },
+  "Tascam": {
+    audios: []
+  },
+  # starts lighting brands
+  "Acuity Brands": {
+    lightings: []
+  },
+  "Aputure": {
+    lightings: []
+  },
+  "Bolt": {
+    lightings: []
+  },
+  "Godox": {
+    lightings: []
+  },
+  "Profoto": {
+    lightings: []
+  },
+  "Elinchrom": {
+    lightings: []
+  },
+  "Broncolor": {
+    lightings: []
+  },
+  "Dynalite": {
+    lightings: []
+  },
+  "Luxli": {
+    lightings: []
+  },
+  "Litepanels": {
+    lightings: []
+  },
+  "Lowel": {
+    lightings: []
+  },
+  "Light & Motion": {
+    lightings: []
+  },
+  "Westcott": {
+    lightings: []
+  },
+  "Kino Flo": {
+    lightings: []
+  },
+  "Mountdog": {
+    lightings: []
+  },
+  "Esmart": {
+    lightings: []
+  },
+  "StudiFX": {
+    lightings: []
+  },
+  "Esddi": {
+    lightings: []
+  },
+  # starts tripod
+  "Manfrotto": {
+    tripods: []
+  },
+  "Gitzo": {
+    tripods: []
+  },
+  "Really Right Stuff": {
+    tripods: []
+  },
+  "Induro": {
+    tripods: []
+  },
+  "Feisol": {
+    tripods: []
+  },
+  "Oben": {
+    tripods: []
+  },
+  "Peak Design": {
+    tripods: []
+  },
+  "3 Legged Thing": {
+    tripods: []
+  },
+  "Sirui": {
+    tripods: []
+  },
+  "Vanguard": {
+    tripods: []
+  },
+  "Slik": {
+    tripods: []
+  },
+  "Zomei": {
+    tripods: []
+  },
+  "Neewer": {
+    tripods: []
+  },
+  "Benro": {
+    tripods: []
+  },
+  "Mefoto": {
+    tripods: []
+  },
+  "Joby": {
+    tripods: []
+  },
+  "Dolica": {
+    tripods: []
+  },
+  "Bonfoto": {
+    tripods: []
+  },
+  "Magnus": {
+    tripods: []
+  },
+  "K&F": {
+    tripods: []
+  },
+  # starts stabilizers
+  "Yuneec": {
+    stabilizers: []
+  },
+ "UVify": {
+    stabilizers: []
+  },
+  "Hubsan": {
+    stabilizers: []
+  },
+  "Parrot": {
+    stabilizers: []
+  },
+  "Autel Robotics": {
+    stabilizers: []
+  },
+  "Air Hogs": {
+    stabilizers: []
+  },
+  "Walkera": {
+    stabilizers: []
+  },
+  "FreeFly": {
+    stabilizers: []
+  },
+  "High Great Drones": {
+    stabilizers: []
+  },
+  "Intel": {
+    stabilizers: []
+  },
+  "AEE": {
+    stabilizers: []
+  },
+  "Ryze": {
+    stabilizers: []
+  }
+}
