f757ac31e90d6f410a0295f7c0e46ae427df9306
Michele Comolli <mikicomolli@gmail.com>
2021-01-16 02:48:20 +0100

    Brand and MountType seeded

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,6 @@
 require 'date'
 require 'faker'
+require_relative 'brands_hash.rb'
 
 
 User.create(email:"lw@email.com", password: "123456", first_name:"Lily", last_name:"Walch", address:"Lech", phone_number:"+436648983164", birth_date: Date.new(2001, 1, 30), identity_number: "123456", issuance_country: "Austria")
@@ -22,247 +23,22 @@ puts "Creating 50 users"
     )
 end
 
-BRANDS = {
-  "Arri": {
-    cameras: [],
-    lenses: [],
-    mount_types: ['PL']
-  },
-  "Canon": {
-    cameras: [],
-    lenses: [],
-    mount_types: ["EF", "RF", "EF-S", "EF-M"]
-  },
-  "DJI": {
-    cameras: [],
-    lenses: [],
-    stabilizers: [],
-  },
-  "Fujifilm": {
-    cameras: [],
-    lenses: [],
-    mount_types: [ "Fuji G", "Fuji X" ]
-  },
-  "GoPro": {
-    cameras: [],
-    lenses: [],
-  },
-  "Hasselblad": {
-    cameras: [],
-    lenses: [],
-    mount_types: ["Hasselblad X", "Hasselblad H"]
-  },
-  "Insta360": {
-    cameras: [],
-    lenses: [],
-  },
-  "Leica": {
-    cameras: [],
-    lenses: [],
-    mount_types: ["L", "M", "R"]
-  },
-  "Nikon": {
-    cameras: [],
-    lenses: [],
-    mount_types: ["F", "S", "1", "Z"]
-  },
-  "Olympus": {
-    cameras: [],
-    lenses: [],
-    mount_types: ["Pen F", "OM", "Four Thirds", "Micro Four Thirds (MFT)"]
-  },
-  "Panasonic": {
-    cameras: [],
-    lenses: [],
-    mount_types: [ "Micro Four Thirds (MFT)" ]
-  },
-  "Pentax": {
-    cameras: [],
-    lenses: [],
-    mount_types: ["K", "Q"]
-  },
-  "Sony": {
-    cameras: [],
-    lenses: [],
-    mount_types: ["E", "EX", "A"]
-  },
-  "Sigma": {
-    cameras: [],
-    lenses: [],
-  },
-  # start audio brands
-  "Diety": {
-    audios: []
-  },
-  "Rode": {
-    audios: []
-  },
-  "Blue Yeti": {
-    audios: []
-  },
-  "Audio Technica": {
-    audios: []
-  },
-  "Sennheiser": {
-    audios: []
-  },
-  "Countryman": {
-    audios: []
-  },
-  "Tascam": {
-    audios: []
-  },
-  # starts lighting brands
-  "Acuity Brands": {
-    lightings: []
-  },
-  "Aputure": {
-    lightings: []
-  },
-  "Bolt": {
-    lightings: []
-  },
-  "Godox": {
-    lightings: []
-  },
-  "Profoto": {
-    lightings: []
-  },
-  "Elinchrom": {
-    lightings: []
-  },
-  "Broncolor": {
-    lightings: []
-  },
-  "Dynalite": {
-    lightings: []
-  },
-  "Luxli": {
-    lightings: []
-  },
-  "Litepanels": {
-    lightings: []
-  },
-  "Lowel": {
-    lightings: []
-  },
-  "Light & Motion": {
-    lightings: []
-  },
-  "Westcott": {
-    lightings: []
-  },
-  "Kino Flo": {
-    lightings: []
-  },
-  "Mountdog": {
-    lightings: []
-  },
-  "Esmart": {
-    lightings: []
-  },
-  "StudiFX": {
-    lightings: []
-  },
-  "Esddi": {
-    lightings: []
-  },
-  # starts tripod
-  "Manfrotto": {
-    tripods: []
-  },
-  "Gitzo": {
-    tripods: []
-  },
-  "Really Right Stuff": {
-    tripods: []
-  },
-  "Induro": {
-    tripods: []
-  },
-  "Feisol": {
-    tripods: []
-  },
-  "Oben": {
-    tripods: []
-  },
-  "Peak Design": {
-    tripods: []
-  },
-  "3 Legged Thing": {
-    tripods: []
-  },
-  "Sirui": {
-    tripods: []
-  },
-  "Vanguard": {
-    tripods: []
-  },
-  "Slik": {
-    tripods: []
-  },
-  "Zomei": {
-    tripods: []
-  },
-  "Neewer": {
-    tripods: []
-  },
-  "Benro": {
-    tripods: []
-  },
-  "Mefoto": {
-    tripods: []
-  },
-  "Joby": {
-    tripods: []
-  },
-  "Dolica": {
-    tripods: []
-  },
-  "Bonfoto": {
-    tripods: []
-  },
-  "Magnus": {
-    tripods: []
-  },
-  "K&F": {
-    tripods: []
-  },
-  # starts stabilizers
-  "Yuneec": {
-    stabilizers: []
-  },
- "UVify": {
-    stabilizers: []
-  },
-  "Hubsan": {
-    stabilizers: []
-  },
-  "Parrot": {
-    stabilizers: []
-  },
-  "Autel Robotics": {
-    stabilizers: []
-  },
-  "Air Hogs": {
-    stabilizers: []
-  },
-  "Walkera": {
-    stabilizers: []
-  },
-  "FreeFly": {
-    stabilizers: []
-  },
-  "High Great Drones": {
-    stabilizers: []
-  },
-  "Intel": {
-    stabilizers: []
-  },
-  "AEE": {
-    stabilizers: []
-  },
-  "Ryze": {
-    stabilizers: []
-  }
-}
+#brand & mount_types seeds
+BRANDS.each do |key, value|
+  puts "creating #{key} brand ..."
+  Brand.create(
+    name: key,
+    product_types: value[:product_types]
+    )
+
+  unless value[:mount_types].nil?
+    puts "creating Mount Types for #{Brand.last.name}"
+    value[:mount_types].each do |mount_type|
+      MountType.create(
+        brand:Brand.last,
+        code: mount_type
+        )
+    end
+  end
+
+end
