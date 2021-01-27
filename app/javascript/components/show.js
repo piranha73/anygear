b6540cb309253023130e052b5e9595e084ed4e18
Lily Walch <lily.walch@gmail.com>
2021-01-27 21:29:05 +0100

    show page

--- app/javascript/components/show.js
+++ app/javascript/components/show.js
@@ -1,7 +1,7 @@
 const productSpecs = () => {
   const specsHeader = document.querySelector('.specs')
   const specsText = document.querySelector('.products-specs')
-  // const icon = document.querySelector('.arrow1');
+  const icon = document.querySelector('.arrow1');
 
   specsText.style.display = "none";
 
@@ -9,10 +9,24 @@ const productSpecs = () => {
   specsHeader.addEventListener('click', () => {
     if (specsText.style.display === "none") {
       specsText.style.display = "block";
-      // icon.classList.toggle('arrow1.up');
+      icon.classList.remove('arrow1');
+      icon.classList.add('arrow1-up');
     } else {
       specsText.style.display = "none";
-      // icon.classList.toggle('arrow1');
+      icon.classList.remove('arrow1-up');
+      icon.classList.add('arrow1');
+    }
+  })
+
+   icon.addEventListener('click', () => {
+    if (specsText.style.display === "none") {
+      specsText.style.display = "block";
+      icon.classList.remove('arrow1');
+      icon.classList.add('arrow1-up');
+    } else {
+      specsText.style.display = "none";
+      icon.classList.remove('arrow1-up');
+      icon.classList.add('arrow1');
     }
   })
 
@@ -21,14 +35,31 @@ const productSpecs = () => {
 const productDetails = () => {
   const productsHeader = document.querySelector('.details')
   const productsText = document.querySelector('.products-details')
+  const icon = document.querySelector('.arrow2');
 
   productsText.style.display = "none";
 
   productsHeader.addEventListener('click', () => {
     if (productsText.style.display === "none") {
       productsText.style.display = "block";
+      icon.classList.remove('arrow2');
+      icon.classList.add('arrow2-up');
     } else {
       productsText.style.display = "none";
+      icon.classList.remove('arrow2-up');
+      icon.classList.add('arrow2');
+    }
+  })
+
+  icon.addEventListener('click', () => {
+    if (productsText.style.display === "none") {
+      productsText.style.display = "block";
+      icon.classList.remove('arrow2');
+      icon.classList.add('arrow2-up');
+    } else {
+      productsText.style.display = "none";
+      icon.classList.remove('arrow2-up');
+      icon.classList.add('arrow2');
     }
   })
 
@@ -37,14 +68,31 @@ const productDetails = () => {
 const productPolicy = () => {
   const policyHeader = document.querySelector('.policy')
   const policyText = document.querySelector('.products-policy')
+  const icon = document.querySelector('.arrow3');
 
   policyText.style.display = "none";
 
   policyHeader.addEventListener('click', () => {
     if (policyText.style.display === "none") {
       policyText.style.display = "block";
+      icon.classList.remove('arrow3');
+      icon.classList.add('arrow3-up');
     } else {
       policyText.style.display = "none";
+      icon.classList.remove('arrow3-up');
+      icon.classList.add('arrow3');
+    }
+  })
+
+  icon.addEventListener('click', () => {
+    if (policyText.style.display === "none") {
+      policyText.style.display = "block";
+      icon.classList.remove('arrow3');
+      icon.classList.add('arrow3-up');
+    } else {
+      policyText.style.display = "none";
+      icon.classList.remove('arrow3-up');
+      icon.classList.add('arrow3');
     }
   })
 
@@ -98,45 +146,7 @@ const imageCarousel = () => {
 
 };
 
-// const productSpecs = () => {
-//   const specsHeader = document.querySelector('.specs')
-//   const specsText = document.querySelector('.products-specs')
 
-//   specsText.style.display = "none";
-
-//   specsHeader.addEventListener('click', () => {
-//     if (specsText.style.display === "none") {
-//       specsText.style.display = "block";
-//     } else {
-//       specsText.style.display = "none";
-//     }
-//   })
-
-// };
-
-
-// $(".rotate").click(function () {
-//     $(this).toggleClass("down");
-// })
-
-  // const arrow1 = document.querySelector(".rotate1")
-
-  // arrow1.addEventListener('click', () => {
-  //   this.toggleClass("down")
-  // });
-
-// const rotatingArrow1 = () => {
-
-  // const div = document.getElementById('.icon-container1');
-  // const icon = document.getElementById('.fas fa-angle-down arrow1');
-
-
-  //   div.addEventListener('click', () => {
-  //   icon.classList.toggle('arrow1-up');
-
-
-  //   });
-// };
 
 
 
