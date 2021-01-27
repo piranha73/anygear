92f183a39b71e0afbd25b44b32a9271c67e7d26a
Michele Comolli <58515733+piranha73@users.noreply.github.com>
2021-01-27 14:47:03 +0100

    Revert "added links to Burger navbar... but it's not quite correct"

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,8 +4,4 @@ class Product < ApplicationRecord
   belongs_to :brand
   has_one_attached :cover_picture
   has_many_attached :uploaded_pictures
-
-  def locate(productable_type)
-    Product.where(productable_type: productable_type)
-  end
 end
