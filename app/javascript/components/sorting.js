3f06d4fb872140d08b63634a72aa3808efbc0523
Michael Tung <tung@hotmail.it>
2021-01-28 16:37:42 +0100

    changes made with lily and michele

--- app/javascript/components/sorting.js
+++ app/javascript/components/sorting.js
@@ -2,17 +2,17 @@ const toggleSorting = () => {
   const filterButton = document.querySelector('.button-filter');
   const filter = document.querySelector('.filter');
 
-  filterButton.addEventListener('click', () => {
-    console.log(filterButton);
-    console.log(filter);
-    if(filter.style.display === 'none') {
-      filter.style.display = 'block';
-      filterButton.innerHTML = 'filter -';
-    } else {
-      filter.style.display = 'none';
-      filterButton.innerHTML = 'filter +';
-    }
-  })
-};
+  if(filterButton) {
+    filterButton.addEventListener('click', () => {
+      if(filter.style.display === 'none') {
+        filter.style.display = 'block';
+        filterButton.innerHTML = 'filter -';
+      } else {
+        filter.style.display = 'none';
+        filterButton.innerHTML = 'filter +';
+      }
+    });
+  }
+}
 
 export { toggleSorting };
